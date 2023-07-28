345ab71a21bd43ef8f5329cdd7c26c556c063015
DustinHeaps <60202144+DustinHeaps@users.noreply.github.com>
2023-07-27 21:33:36 -0400

    fixed mobile styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,14 @@ import Username from "../features/user/Username";
 const Header = () => {
   return (
     <header className='flex items-center justify-between border-b border-stone-200 bg-yellow-400 px-4 py-3 uppercase sm:px-6'>
+    
       <Link to='/' className='tracking-widest flex items-center'>
-        <img className='mr-2 w-12' src='/logo.png' alt='' />
+        <img className='mr-2 w-8 sm:w-12' src='/logo.png' alt='' />
         The Pizza Co.
       </Link>
-      <SearchOrder />
+      
+      
+      <SearchOrder  />
       <Username />
     </header>
   );
